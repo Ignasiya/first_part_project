6011c6bae263c515f88e43cb78c759f516a6cab9
Ignasiya <ignasiya93@gmail.com>
2024-08-20 12:50:00 +0300

    class Swiper

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -28,28 +28,99 @@ elsNavigateShowToggle?.forEach((el) => {
   });
 });
 
-let mySwiper;
+class SliderGallery {
+  constructor(options = {}) {
+    this.options = options;
+    this.breakpoints = {
+      sm: 576,
+      md: 768,
+      lg: 992,
+      xl: 1200,
+      xxl: 1400,
+    };
+    this.slider = null;
 
-function initSwiper() {
-  if (window.innerWidth < 768) {
-    if (!mySwiper) {
-      mySwiper = new Swiper(".mySwiper", {
-        spaceBetween: 24,
-        width: 272,
-        height: 280,
-        pagination: {
-          el: ".swiper-pagination",
-          dynamicBullets: true,
-        },
-      });
+    if (this.slider === null) {
+      window.addEventListener("resize", this.handleResizeWindow.bind(this));
+      this.handleResizeWindow(window);
     }
-  } else {
-    if (mySwiper) {
-      mySwiper.destroy(true, true);
-      mySwiper = null;
+  }
+
+  get _breakpoints() {
+    return this._breakpoints;
+  }
+
+  set _breakpoints(val) {
+    return (this._breakpoints = val);
+  }
+
+  get _options() {
+    return this.options;
+  }
+
+  set _options(val) {
+    return (this._options = val);
+  }
+
+  handleResizeWindow(event) {
+    const { innerWidth = 0 } = event?.target || event;
+
+    if (innerWidth > this.breakpoints[this.options.breakpoint]) {
+      this.destroy();
+      return;
     }
+    this.init();
+  }
+
+  init() {
+    if (this.slider !== null) return;
+    this.slider = new Swiper(this.options.el, {
+      ...(this.options.sliderOptions || {}),
+    });
+  }
+
+  destroy() {
+    if (this.slider === null) return;
+    this.slider.destroy();
+    this.slider = null;
   }
 }
 
-window.addEventListener("resize", initSwiper);
-initSwiper();
+new SliderGallery({
+  el: ".mySwiper",
+  breakpoint: "md",
+  sliderOptions: {
+    spaceBetween: 24,
+    width: 272,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+  },
+});
+
+// let mySwiper;
+
+// function initSwiper() {
+//   if (window.innerWidth < 768) {
+//     if (!mySwiper) {
+//       mySwiper = new Swiper(".mySwiper", {
+//         spaceBetween: 24,
+//         width: 272,
+//         height: 280,
+//         pagination: {
+//           el: ".swiper-pagination",
+//           dynamicBullets: true,
+//         },
+//       });
+//     }
+//   } else {
+//     if (mySwiper) {
+//       mySwiper.destroy(true, true);
+//       mySwiper = null;
+//     }
+//   }
+// }
+
+// window.addEventListener("resize", initSwiper);
+// initSwiper();
